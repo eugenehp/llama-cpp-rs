a439aeb65814f454f4bc378c165b1d12a6847c41
eugenehp <eugene@reactivelions.com>
2026-03-29 20:45:41 -0700

    0.2.17

--- llama-cpp-4/src/lib.rs
+++ llama-cpp-4/src/lib.rs
@@ -414,7 +414,7 @@ pub fn flash_attn_type_name(flash_attn_type: i32) -> String {
 /// Panics if the returned string is not valid UTF-8.
 #[must_use]
 pub fn model_meta_key_str(key: u32) -> String {
-    let c_str = unsafe { llama_cpp_sys_4::llama_model_meta_key_str(key) };
+    let c_str = unsafe { llama_cpp_sys_4::llama_model_meta_key_str(key.try_into().unwrap()) };
     let c_str = unsafe { std::ffi::CStr::from_ptr(c_str) };
     c_str.to_str().expect("meta_key_str is not valid UTF-8").to_owned()
 }
